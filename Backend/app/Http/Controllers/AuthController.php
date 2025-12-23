6c34e952e8e099961ed2b3f6f47318b0836d290b
TonNomGitHub <ton@email.com>
2025-12-23 21:08:09 +0100

    Ajout l'authentification coté  backend

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    
+public function register(RegisterRequest $request)
+    {
+    $user = User::create([
+        'nom' => $request->nom,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        'role' => 'utilisateur',
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'message' => 'Inscription réussie',
+        'user' => $user,
+        'token' => $token,
+    ]);
+
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Email ou mot de passe incorrect'], 401);
+    }
+
+    $token = $user->createToken('token')->plainTextToken;
+
+    return response()->json([
+        'message' => 'Connexion réussie',
+        'user' => $user,
+        'token' => $token,
+    ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Déconnexion réussie',
+        ]);
+    }
+
 }
